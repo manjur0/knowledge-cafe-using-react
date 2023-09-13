7cc1e442274fd20c198272106cd7aae0757aabda
manjur0 <sfmanjur21@gmail.com>
2023-09-13 06:36:58 +0600

    knowledge cafe website data ready for building site

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-bold mt-10'>Knowledge cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
