6b66b613566c5866e7ad75bc8ef27a2f0abaf8d5
manjur0 <sfmanjur21@gmail.com>
2023-09-13 11:28:19 +0600

    blog page res done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,11 @@
-
+import profile from "../../assets/images/profile.png"
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-4xl text-center font-bold mt-10'>Knowledge cafe</h1>
+        <div className="container mx-auto  ">
+            <div className=" flex justify-between items-center border-b-2 pb-3">
+                <h1 className='text-4xl font-bold mt-10 '>Knowledge cafe</h1>
+                <img src={profile} alt="" />
+            </div>
         </div>
     );
 };
