06d0dd51cee6455e20033e116868d88eee86baf7
manjur0 <sfmanjur21@gmail.com>
2023-09-13 14:56:05 +0600

    blogs added

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import Header from './Components/Header/Header'
 function App() {
 
   return (
-    <>
+    <div className='container mx-auto'>
       <Header></Header>
       <div className=' md:flex'>
         <Blogs></Blogs>
         <Bookmarks></Bookmarks>
       </div>
-    </>
+    </div>
   )
 }
 
